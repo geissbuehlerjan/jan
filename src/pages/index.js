909e6e0d204f4e0f6bd4df76abb417f1840a5445
geissbuehlerjan <jan.geissbuehler@student.unisg.ch>
2022-02-14 22:09:50 +0100

    stripe promise updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,7 @@ import SEO from "../components/seo"
 import Skus from "../components/Products/Skus"
 import CartOverview from "../components/CartOverview"
 
-import { loadStripe } from "@stripe/stripe-js"
 import { CartProvider } from "use-shopping-cart"
-
-const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
 
 const CartExample = () => (
   <Layout>
@@ -20,8 +17,9 @@ const CartExample = () => (
       <a href="https://use-shopping-cart.netlify.app/">use-shopping-cart</a>
     </h2>
     <CartProvider
-      mode="client-only"
-      stripe={stripePromise}
+      mode="payment"
+      cartMode="client-only"
+      stripe={process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}
       successUrl={`${window.location.origin}/page-2/`}
       cancelUrl={`${window.location.origin}/`}
       currency="CHF"
