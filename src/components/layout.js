ca8a71f763898e2a56e18590a4082e5490897594
geissbuehlerjan <jan.geissbuehler@student.unisg.ch>
2022-03-08 16:01:32 +0100

    Header

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,11 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header  />
+      
       <div
         style={{
+          
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
@@ -39,9 +41,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
